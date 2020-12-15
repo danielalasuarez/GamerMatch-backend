8b0e10366810bfaab1a3090655aa553c2a0f3e1d
danielalasuarez <69167761+danielalasuarez@users.noreply.github.com>
2020-12-15 11:56:35 -0500

    route set up and working

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 //NOTE: in the package.json we added "type": "module" in order to get ES6 and be able to use import instead of require
 //we also added the start script
 
+import express from 'express'
+import mongoose from 'mongoose'
+
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001
+
+//Middleware
+//DB config
+//API endpoints
+
+app.get('/', (req, res) => res.status(200).send('working'));
+
+//Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
